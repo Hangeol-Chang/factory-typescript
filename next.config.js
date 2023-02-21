9b3271544103f126cb48b15a90d61a46e43a2399
ChangHangeol <hgchang1@naver.com>
2023-02-21 22:30:35 +0900

    init setting

--- next.config.js
+++ next.config.js
@@ -1,6 +1,18 @@
 /** @type {import('next').NextConfig} */
+
+const debug = process.env.NODE_ENV !== "production";
+const repository = "https://hangeol-chang.github.io/factory-typescript";
+
 const nextConfig = {
   reactStrictMode: true,
+  assetPrefix: !debug ? `${repository}/` : "/", 
+  basePath : !debug ? `factory-typescript` : "",
+  trailingSlash: true,
+
+  images: {
+    loader: 'akamai',
+    path: '/',
+  }
 }
 
 module.exports = nextConfig
